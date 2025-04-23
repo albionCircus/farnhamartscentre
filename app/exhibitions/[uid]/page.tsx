32766c496cd902ecedca04c3f8078cf40d3b0b07
AlbionCircus <hello@albioncircus.com>
2025-04-23 12:26:35 +0100

    Article styling

--- app/exhibitions/[uid]/page.tsx
+++ app/exhibitions/[uid]/page.tsx
@@ -1,10 +1,11 @@
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { asImageSrc } from "@prismicio/client";
-import { SliceZone } from "@prismicio/react";
+import { PrismicRichText, SliceZone } from "@prismicio/react";
 import Bounded from "@/app/components/Bounded";
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
+import { PrismicNextImage } from "@prismicio/next";
 
 type Params = { uid: string };
 
@@ -15,8 +16,13 @@ export default async function Page({ params }: { params: Promise<Params> }) {
 
   return (
     <Bounded>
-        <article>
-            <h1>{page.data.heading}</h1>
+        <article className="justify-center margin0auto w-full max-w-[800px] max-w">
+            <h1 className="text-charcoal">{page.data.heading}</h1>
+            <PrismicNextImage field={page.data.image} className="py-3" />                
+            <h4 className="text-charcoal">{page.data.description}</h4>
+            <p className="my-3">{new Date(page.data.original_date || Date.now()).toLocaleDateString("en-GB")}</p>
+            <PrismicRichText field={page.data.article} />
+            <p><em>{page.data.author}</em></p>
         </article>
         <SliceZone slices={page.data.slices} components={components} />
     </Bounded>
