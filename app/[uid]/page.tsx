2fe8ff6295c3cbdff3c0b6ea20141225f763bb98
AlbionCircus <hello@albioncircus.com>
2025-05-13 16:44:37 +0100

    ISR Test

--- app/[uid]/page.tsx
+++ app/[uid]/page.tsx
@@ -1,4 +1,4 @@
-export const revalidate = 60; // Revalidate this page every 60 seconds
+// export const revalidate = 60; // Revalidate this page every 60 seconds
 
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
