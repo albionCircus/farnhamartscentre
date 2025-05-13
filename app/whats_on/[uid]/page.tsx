e4a6785fed6983541e888833e0d1c53d7910397d
AlbionCircus <hello@albioncircus.com>
2025-05-13 14:55:00 +0100

    ISR implementation

--- app/whats_on/[uid]/page.tsx
+++ app/whats_on/[uid]/page.tsx
@@ -1,3 +1,5 @@
+export const revalidate = 60; // Add revalidate for ISR (Incremental Static Regeneration)
+
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { asImageSrc } from "@prismicio/client";
