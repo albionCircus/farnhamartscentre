1b8b6da0233a55809b465ba78bf0dd027188ec09
AlbionCircus <hello@albioncircus.com>
2025-05-21 11:44:29 +0100

    Footer updates

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,26 +5,35 @@ import Image from "next/image";
 export default async function Footer() {
     const client = createClient();
     const home = await client.getSingle("home");
-    return <footer className="bg-realTeal mt-8 py-6 flex flex-row justify-center items-center">
-            <p className="text-white text-center">
-                <small>
-                    {home.data.meta_title} &copy; {new Date().getFullYear()} 
-                    <span className="hidden sm:inline text-white px-2">|</span>
-                    <br className="block sm:hidden" />
-                    <span>
-                        <Link href={"https://albioncircus.com/"}>
-                            Site by Albion Circus
-                            <Image
-                                src="/albionCircusWhite.svg"
-                                alt="Albion Circus"
-                                width={18}
-                                height={18}
-                                quality={100}
-                                className="inline-block ml-1 mt-0 sm:-mt-1"
-                            />
-                        </Link>
-                    </span>
-                </small>
-           </p>
+    return <footer className="">
+            <div className="flex flex-row justify-center items-center bg-gray-200 mt-8 py-5">
+                <Link href="/privacy" className=""><p className="text-realTeal text-center">Privacy Policy</p></Link>
+                <span className="px-2">|</span>
+                <Link href="/accessibility" className=""><p className="text-realTeal text-center">Accessibility</p></Link>
+                <span className="px-2">|</span>
+                <Link href="/terms-and-conditions" className=""><p className="text-realTeal text-center">Terms & Conditions</p></Link>
+            </div>
+            <div className="bg-realTeal py-3 flex flex-row justify-center items-center">
+                <p className="text-white text-center">
+                    <small>
+                        {home.data.meta_title} &copy; {new Date().getFullYear()} 
+                        <span className="hidden sm:inline text-white px-2">|</span>
+                        <br className="block sm:hidden" />
+                        <span>
+                            <Link href={"https://albioncircus.com/"}>
+                                Site by Albion Circus
+                                <Image
+                                    src="/albionCircusWhite.svg"
+                                    alt="Albion Circus"
+                                    width={18}
+                                    height={18}
+                                    quality={100}
+                                    className="inline-block ml-1 mt-0 sm:-mt-1"
+                                />
+                            </Link>
+                        </span>
+                    </small>
+                </p>
+            </div>
     </footer>;
 }
